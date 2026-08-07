72bb8f382a9e764af5bc4feb7d7eb4e5746e23b6
Emirhan <emirhan.bda0@gmail.com>
2026-08-07 15:25:24 +0300

    d

--- src/links/links.module.ts
+++ src/links/links.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { LinksService } from './links.service';
 import { LinksController } from './links.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Link, LinkSchema } from './link.schema';
 
 
 @Module({
+  imports:[
+    MongooseModule.forFeature([{name:Link.name,schema:LinkSchema}])
+  ],
   controllers: [LinksController],
   providers: [LinksService],
 })
