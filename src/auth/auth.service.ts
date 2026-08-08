1445e14722e57fea0efb3d3e1c520018ff5fdad3
Emirhan <emirhan.bda0@gmail.com>
2026-08-08 14:28:13 +0300

    s

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { RegisterDto } from './dto/register-dto';
 import { LoginDto } from './dto/login-dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt'
 
 @Injectable()
