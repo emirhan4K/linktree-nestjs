2e8c30a1fbe6010243f8a4b53e7ae124ff0e36d6
Emirhan <emirhan.bda0@gmail.com>
2026-08-20 13:33:23 +0300

    s

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards,} from '@nestjs/common';
+import { UserService } from './user.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guards';
 
 @Controller('user')
 export class UserController {
-  
+  constructor(private userService:UserService){}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Req() req){
+    return this.userService.getProfile(req.user.id)
+  }
+
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schema/user.schema';
 import { Model } from 'mongoose';
@@ -8,5 +8,15 @@ import { Model } from 'mongoose';
 export class UserService  {
   constructor(@InjectModel(User.name)
   private userModel:Model<any>){}
+
+  async getProfile(userId:string){
+    const user = await this.userModel.findById(userId).select('-password')
+    if(!user){
+      throw new NotFoundException('Kullanıcı bulunamadı!')
+    }
+    return user;
 }
+
+}
+
 
